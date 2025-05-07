6b85ff660d74664884894bd9c8a72b3bd378940e
Graham <45939582+Ruthenmoir@users.noreply.github.com>
2025-05-07 11:01:21 +0930

    Initial Start point for Assignment 3 Question 1

---
+++ Assignment 3 Question 1.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter.messagebox
+
+
+root = Tk()
+root.title("Group 2 Assignment 3")
+
+theLabel = Label(root, text="Welcome to our Image Manipulator")
+theLabel.pack()
+
+#Image processing Section
+
+
+#GUI Manager Section
+
+
+#Mouse Interaction Section
+
+
+#History Manager Section for Undo and Redo
+
+
+
+
+
+root.mainloop()
+
+
+
